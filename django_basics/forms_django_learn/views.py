c64c2da68d2f17b5784e09236a0f8aa749448770
SREEJITH-P-B <33229709+SREEJITH-P-B@users.noreply.github.com>
2020-10-20 03:30:43 +0000

    1.created a simple form which acceptinput from user and then it displaying on the same page 2.tdo:store the details on database and display whole results

--- django_basics/forms_django_learn/views.py
+++ django_basics/forms_django_learn/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 def home(request):
     return render(request,'forms_django_learn/index.html')
 def details(request):
-    return render(request,'forms_django_learn/details.html')
+    if request.method=='POST':
+        fname=request.POST.get('fname')
+        lname=request.POST.get('lname')
+        stream=request.POST.get('stream')
+        context = {
+        "fname":fname,
+        "lname": lname,
+        "stream": stream,
+        }
+        return render(request,'forms_django_learn/details.html',context)
+    else:
+        context = {
+        "fname":"",
+        "lname": "",
+        "stream": "",
+        }
+        return render(request,'forms_django_learn/details.html',context)
